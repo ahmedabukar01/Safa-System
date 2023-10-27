511efb28f1fac253851b6cf28c71be701bb00e37
ahmed nur <ahmedlearn01@gmail.com>
2023-10-27 09:06:37 +0300

    cleared state

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,20 +1,9 @@
 'use client'
-import { useEffect, useState } from 'react'
+import * as React from 'react'
 import { useAppContext } from '../lib/AppContext'
 
 export default function Login() {
     const {state, setState}: any = useAppContext()
-    const [my, setMY] = useState({
-        ali: 'aww',
-        ma: "haa"
-    })
-
-    useEffect(() => {
-        setState({...state, name: "ahmed"});
-    }, [])
-    
-    console.log(state)
-
 
   return (
     <div>page</div>
