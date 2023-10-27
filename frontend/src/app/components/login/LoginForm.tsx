c127769c342a32fa52dc7255b3cb107dca63cfc1
ahmed nur <ahmedlearn01@gmail.com>
2023-10-27 09:54:24 +0300

    login page is set

---
+++ frontend/src/app/components/login/LoginForm.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import { Button, Checkbox, Col, Form, Input, Row, Space , Typography} from 'antd';
+
+const { Title } = Typography
+export default function LoginForm() {
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+      };
+      
+      const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+      };
+
+  return (
+    <div>
+        <div style={{margin: '4em auto', textAlign: "center"}} >
+        <Title level={1}>Welcome To Safa System Online System</Title>
+        </div>
+    <Form
+    name="login form"
+    // initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    style={{ maxWidth: 600, maxHeight: 900, display: 'flex', margin: 'auto', flexDirection: "column", justifyContent: "center", alignContent: "center" }}
+  >
+    <Row >
+      <Col span={24}>
+        <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: 'Please input your email!' }]}
+        >
+        <Input />
+      </Form.Item>
+      </Col>
+    </Row>
+    <Row>
+      <Col span={24}>
+        <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+        <Input.Password />
+      </Form.Item>
+      </Col>
+    </Row>
+
+    {/* <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item> */}
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+
+  </Form>
+    </div>
+  )
+}
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 import * as React from 'react'
 import { useAppContext } from '../lib/AppContext'
+import LoginForm from '../components/login/LoginForm'
 
 export default function Login() {
     const {state, setState}: any = useAppContext()
 
   return (
-    <div>page</div>
+    <LoginForm />
   )
 }
