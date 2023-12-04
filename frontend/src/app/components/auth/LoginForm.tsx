746c4c020aa3b19b834905ca5a0df39dd38ed4d5
ahmed nur <ahmedlearn01@gmail.com>
2023-12-04 18:47:31 +0300

    payment details

--- frontend/src/app/components/auth/LoginForm.tsx
+++ frontend/src/app/components/auth/LoginForm.tsx
@@ -13,7 +13,17 @@ export default function LoginForm({onSubmit}: {onSubmit: any}) {
       };
 
   return (
-    <div>
+    <div style={{
+      width: 600,
+      height: 900,
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "column",
+      alignContent: "cneter",
+      alignItems: "center", 
+      margin: "auto",
+      // background: "radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(90,103,201,0.9533000700280112) 10%, rgba(36,31,207,0.8860731792717087) 74%, rgba(0,212,255,1) 100%)"
+    }}>
         <div style={{margin: '4em auto', textAlign: "center"}} >
         <Title level={1}>Welcome To Safa Online System</Title>
         </div>
@@ -22,12 +32,12 @@ export default function LoginForm({onSubmit}: {onSubmit: any}) {
     // initialValues={{ remember: true }}
     onFinish={onFinish}
     onFinishFailed={onFinishFailed}
-    style={{ maxWidth: 600, maxHeight: 900, display: 'flex', margin: 'auto', flexDirection: "column", justifyContent: "center", alignContent: "center" }}
+    // style={{ maxWidth: 600, maxHeight: 900, display: 'flex', margin: 'auto', flexDirection: "column", justifyContent: "center", alignContent: "center" }}
   >
     <Row >
       <Col span={24}>
         <Form.Item
-        label="Email"
+        label="Email   "
         name="email"
         rules={[{ required: true, message: 'Please input your email!' }]}
         >
