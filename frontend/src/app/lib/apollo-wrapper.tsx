0278bb5ea72cfcb21e131cbf90457863d375f713
ahmed nur <ahmedlearn01@gmail.com>
2023-12-11 11:12:12 +0300

    env files production added

--- frontend/src/app/lib/apollo-wrapper.tsx
+++ frontend/src/app/lib/apollo-wrapper.tsx
@@ -13,11 +13,12 @@ import {
   SSRMultipartLink,
 } from "@apollo/experimental-nextjs-app-support/ssr";
 import { useCookies } from "next-client-cookies";
-
+console.log('haaa',process.env.BACKEND_URL)
 function makeClient(token: any) {
   const httpLink = new HttpLink({
       // https://studio.apollographql.com/public/spacex-l4uc6p/
-      uri: "http://localhost:8000/graphql",
+      // uri: process.env.NODE_ENV === 'development' ? process.env.BACKEND_DEV_URL : process.env.BACKEND_PROD_URL ,//"http://localhost:8000/graphql",
+      uri: process.env.NEXT_PUBLIC_BACKEND_URL,
       fetchOptions: {cache: "no-store"},
       headers: {
         Authorization: token && `Bearer ${token}`
