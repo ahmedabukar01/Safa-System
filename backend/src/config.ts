ea874bac63b4acb328a1ef56860a73fa475e1b5a
ahmed nur <ahmedlearn01@gmail.com>
2023-12-12 08:22:47 +0300

    added https certificate

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,6 +2,6 @@ export const ALLOWED_HOSTS = [
     "http://64.23.129.26",
     "http://64.23.129.26:8000",
     "http://localhost:3000",
-    "http://localhost:8000/graphql",
+    "https://localhost:8000/graphql",
     "https://studio.apollographql.com",
 ]
