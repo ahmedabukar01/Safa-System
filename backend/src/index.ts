03a3b76e58984618cdcb8929fbfeb2728411d1ef
ahmed nur <ahmedlearn01@gmail.com>
2024-01-12 08:07:00 +0300

    turn of itrospection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -64,7 +64,7 @@ const server = new ApolloServer({
     resolvers, 
     typeDefs,
     csrfPrevention: true,
-    // introspection: false,
+    introspection: false,
     cache: new InMemoryLRUCache({
         maxSize: 1024, // limit the cache size to 1024 MB
     }),
